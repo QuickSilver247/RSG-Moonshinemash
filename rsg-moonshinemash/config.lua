09524f0e7f89ff57cef66cdf724d9f497d0db591
QuickSilver247 <128346898+QuickSilver247@users.noreply.github.com>
2023-03-20 23:11:36 -0500

    Update config.lua

--- rsg-moonshinemash/config.lua
+++ rsg-moonshinemash/config.lua
@@ -5,4 +5,4 @@ Config.Prop = 'p_barrelhalf04x' -- prop used for the moonshine
 Config.JobName = 'police' -- job that can distroy moonshines
 Config.BrewTime = 15000 -- brewtime in milliseconds
 Config.Animation = {}
-Config.ProgressBar = true
+Config.GiveAmountmoonshinemash =
